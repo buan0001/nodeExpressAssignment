9ed2f03d75b1c2cb0039bccdcb7ea5bb94f13031
buan0001 <bustermikkel@gmail.com>
2023-09-05 15:24:05 +0200

    fairly restful

--- index.js
+++ index.js
@@ -8,34 +8,40 @@ app.use(cors());
 const port = 3000;
 
 async function readArtistsFile() {
-  const artists = await fs.readFile("users/artists.json");
-  return JSON.parse(artists);
+  try {const artists = await fs.readFile("users/artists.json"); return JSON.parse(artists);}
+  catch (err){return err}
 }
 
 app.get("/", (req, res) => {
-  res.json("teanstkj");
+  res.json("YEP GET");
 });
 
-app.get("/artists", async (request, response) => {
-  response.json(await readArtistsFile());
-});
-
-app.get("/artists/:artistId", async (request, response) => {
+app.get("/artists", async (req, res) => {
   const artists = await readArtistsFile();
-  const correctartist = artists.find(task => Number(request.params.artistId) == Number(task.id));
-  if (!correctartist) {
-    return response.status(404).json("User not found");
+  if (artists instanceof Error){return res.json(artists)}
+  else {res.json(artists)}
+});
+
+app.get("/artists/:artistId", async (req, res) => {
+  const artists = await readArtistsFile();
+  if (artists instanceof Error) {
+    return res.json(artists);
   }
-  response.json(correctartist);
+  const correctartist = artists.find((task) => Number(req.params.artistId) == Number(task.id));
+  if (!correctartist) {
+    return res.status(404).json("User not found");
+  }
+  res.json(correctartist);
 });
 
 app.put("/artists/:artistId", async (req, res) => {
   const artists = await readArtistsFile();
+  if (artists instanceof Error) {
+    return res.json(artists);
+  }
   const idToLookFor = Number(req.params.artistId);
-  console.log(idToLookFor);
   const body = req.body;
-  let foundArtist = artists.find(artist => idToLookFor == artist.id);
-  console.log("FOUND ARTIST:", foundArtist);
+  let foundArtist = artists.find((artist) => idToLookFor == artist.id);
   if (foundArtist == undefined) {
     return res.status(404).json("Couldn't find an artist with that ID!");
   }
@@ -48,22 +54,57 @@ app.put("/artists/:artistId", async (req, res) => {
   foundArtist.image = body.image;
   foundArtist.shortDescription = body.shortDescription;
   foundArtist.id = idToLookFor;
-  try {
-    await fs.writeFile("users/artists.json", JSON.stringify(artists));
-    res.json(artists);
-  } catch {
-    return res.status(500).json("Internal server error; couldn't update the artist");
+
+  const promise = await writeArtistsFile(artists);
+  res.json(promise);
+});
+
+app.post("/artists", async (req, res) => {
+  const artists = await readArtistsFile();
+  if (artists instanceof Error) {
+    return res.json(artists);
+  }
+  const newArtist = req.body;
+  newArtist.id = new Date().getTime();
+  let uniqueUser = true;
+  for (const artist of artists) {
+    if (artist.name === newArtist.name || artist.id === newArtist.id) {
+      uniqueUser = false;
+      break;
+    }
+  }
+  if (uniqueUser) {
+    artists.push(newArtist);
+    const promise = await writeArtistsFile(artists);
+    res.json(promise);
+  } else {
+    res.json("Artist already exists!");
   }
 });
 
-app.post("/artists", async (request, response) => {
-  const artist = await readArtistsFile;
-  const newartist = request.body;
-  newartist.id = new Date().getTime();
-  artists.push(newartist);
-  response.json(artists);
+app.delete("/artists/:artistID", async (req, res) => {
+  const artists = await readArtistsFile();
+  if (artists instanceof Error) {
+    return res.json(artists);
+  }
+  const filteredList = artists.filter((artist) => artist.id != req.params.artistID);
+  if (filteredList.length == artists.length) {
+    res.json("Couldn't find that ID");
+  } else {
+    const promise = await writeArtistsFile(filteredList);
+    res.json(promise);
+  }
 });
 
 app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`);
 });
+
+async function writeArtistsFile(fileToWrite) {
+  try {
+    await fs.writeFile("users/artists.json", JSON.stringify(fileToWrite));
+    return "Succesful request";
+  } catch (err) {
+    return err;
+  }
+}
