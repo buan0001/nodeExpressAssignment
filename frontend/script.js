17de3168653a57b58dfe6826cf594d950b9d7ceb
buan0001 <bustermikkel@gmail.com>
2023-09-05 19:21:18 +0200

    Working on connect front- and backend

--- frontend/script.js
+++ frontend/script.js
@@ -24,7 +24,7 @@ function showArtists(listOfArtists) {
     grid.insertAdjacentHTML(
       `beforeend`,
       `
-    <article>
+    <article class="artist">
     <div>Artist name: <b>${artist.name}</b></div>
     <div><img src="${artist.image}"></div>
     <div>Birthdate: <b>${artist.birthdate}</b> </div>
@@ -33,15 +33,36 @@ function showArtists(listOfArtists) {
     <div>Labels: <b>${artist.labels}</b></div>
     <div>Website:${artist.website}</div>
     <div>About the artist: <b> ${artist.shortDescription}</b></div>
-    <button>Update the artist</button>
-    <button class="red">DELETE THIS artist</button>
+    <button class="update-btn">Update the artist</button>
+    <button class="red delete-btn">DELETE THIS artist</button>
     </article>
     `
     );
+    document.querySelector("article:last-child .update-btn").addEventListener("click", () => updateArtistClicked(artist));
+    document.querySelector("article:last-child .delete-btn").addEventListener("click", () => deleteArtistClicked(artist));
   }
 }
 
 function createNewArtist(params) {
   console.log("create new artist clicked");
   document.querySelector("#submit-dialog").showModal();
+}
+
+function updateArtistClicked(artist) {
+  console.log("update artist", artist);
+  const form = document.querySelector("#submit-form");
+  let node = document.querySelector("#submit-btn");
+  node.addEventListener("click", test);
+  let clone = node.cloneNode(true);
+  form.appendChild(clone);
+  form.reset();
+  document.querySelector("#submit-dialog").showModal();
+}
+
+function deleteArtistClicked(artist) {
+  console.log("delete artist", artist);
+}
+
+function test(params) {
+  console.log("testing!");
 }
